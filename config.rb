39f39cb8380326f8a721587c51dfee552d96d7ba
Alan Buscaglia <abuscaglia@belatrixsf.com>
2016-11-03 20:55:57 -0300

    new compass config

--- config.rb
+++ config.rb
@@ -2,8 +2,8 @@
 css_dir = "style/css" # by Compass.app 
 sass_dir = "style/scss" # by Compass.app 
 images_dir = "images" # by Compass.app 
-output_style = :expanded # by Compass.app 
+output_style = :compressed # by Compass.app 
 relative_assets = false # by Compass.app 
-line_comments = true # by Compass.app 
+line_comments = false # by Compass.app 
 sass_options = {:debug_info=>false} # by Compass.app 
 sourcemap = true # by Compass.app 
